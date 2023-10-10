213820a0d10d2a749b8e2dddf01b9a8b097bfdde
Federico Vazzoler <federico.vazzoler@cern.ch>
2023-10-10 12:06:47 +0200

    Add helpers for the Z MiNNLO samples

---
+++ make_config_SMP-RunIISummer20UL16wmLHEGEN-00645.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+fragment=SMP-RunIISummer20UL16wmLHEGEN-00645.py
+outputfile=SMP-RunIISummer20UL16wmLHEGEN-00645.root
+
+customize="--customise_commands process.RandomNumberGeneratorService.externalLHEProducer.initialSeed=999"
+customize="${customize}\nprocess.externalLHEProducer.generateConcurrently=True --nThreads 8"
+
+cmsenv
+cmsDriver.py Configuration/SMP-22-010_NanoGen/python/$fragment \
+    --fileout file:$outputfile --mc --eventcontent NANOAODSIM \
+    --datatier NANOAOD --conditions auto:mc --step LHE,GEN,NANOGEN \
+    --python_filename configs/${fragment/cff/cfg} \
+    $customize \
+    -n 30 --no_exec
