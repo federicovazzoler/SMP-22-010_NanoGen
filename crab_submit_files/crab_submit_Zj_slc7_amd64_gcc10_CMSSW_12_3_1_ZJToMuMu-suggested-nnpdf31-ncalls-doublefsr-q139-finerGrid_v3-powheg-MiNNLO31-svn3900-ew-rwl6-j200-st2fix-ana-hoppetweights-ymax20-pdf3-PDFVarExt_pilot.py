282e45463685dd7c1cd8445516ff23f38f1b5930
Federico Vazzoler <federico.vazzoler@cern.ch>
2023-12-13 09:56:08 +0100

    Clean up and ready for CRAB

---
+++ crab_submit_files/crab_submit_Zj_slc7_amd64_gcc10_CMSSW_12_3_1_ZJToMuMu-suggested-nnpdf31-ncalls-doublefsr-q139-finerGrid_v3-powheg-MiNNLO31-svn3900-ew-rwl6-j200-st2fix-ana-hoppetweights-ymax20-pdf3-PDFVarExt_pilot.py
@@ -0,0 +1,24 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = 'ZJToMuMU-H2BugFix-PDFVarExt_pilot'
+config.General.workArea = 'crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.numCores = 1
+config.JobType.maxMemoryMB = 2000
+config.JobType.pluginName = 'PrivateMC'
+config.JobType.psetName = '../configs/Zj_slc7_amd64_gcc10_CMSSW_12_3_1_ZJToMuMu-suggested-nnpdf31-ncalls-doublefsr-q139-finerGrid_v3-powheg-MiNNLO31-svn3900-ew-rwl6-j200-st2fix-ana-hoppetweights-ymax20-pdf3-PDFVarExt_pilot_cfg.py'
+config.JobType.allowUndistributedCMSSW = True
+
+config.Data.outputPrimaryDataset = 'ZJToMuMU-H2BugFix-PDFVarExt_pilot'
+config.Data.splitting = 'EventBased'
+config.Data.unitsPerJob = 10
+NJOBS = 10
+config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
+config.Data.outLFNDirBase = '/store/group/phys_smp/sin2O' 
+config.Data.publication = False
+config.Data.outputDatasetTag = 'NanoGEN'
+
+config.Site.storageSite = 'T2_CH_CERN'
