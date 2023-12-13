613ff6eff17506f8c9ce5b11c8e9fafef02a3c0c
Federico Vazzoler <federico.vazzoler@cern.ch>
2023-12-13 11:49:33 +0100

    Fix typo in proc name

--- crab_submit_files/crab_submit_Zj_slc7_amd64_gcc10_CMSSW_12_3_1_ZJToMuMu-suggested-nnpdf31-ncalls-doublefsr-q139-finerGrid_v3-powheg-MiNNLO31-svn3900-ew-rwl6-j200-st2fix-ana-hoppetweights-ymax20-pdf3-PDFVarExt_pilot.py
+++ crab_submit_files/crab_submit_Zj_slc7_amd64_gcc10_CMSSW_12_3_1_ZJToMuMu-suggested-nnpdf31-ncalls-doublefsr-q139-finerGrid_v3-powheg-MiNNLO31-svn3900-ew-rwl6-j200-st2fix-ana-hoppetweights-ymax20-pdf3-PDFVarExt_pilot.py
@@ -1,7 +1,7 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'ZJToMuMU-H2BugFix-PDFVarExt_pilot'
+config.General.requestName = 'ZJToMuMu-H2BugFix-PDFVarExt_pilot'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = True
@@ -12,7 +12,7 @@ config.JobType.pluginName = 'PrivateMC'
 config.JobType.psetName = '../configs/Zj_slc7_amd64_gcc10_CMSSW_12_3_1_ZJToMuMu-suggested-nnpdf31-ncalls-doublefsr-q139-finerGrid_v3-powheg-MiNNLO31-svn3900-ew-rwl6-j200-st2fix-ana-hoppetweights-ymax20-pdf3-PDFVarExt_pilot_cfg.py'
 config.JobType.allowUndistributedCMSSW = True
 
-config.Data.outputPrimaryDataset = 'ZJToMuMU-H2BugFix-PDFVarExt_pilot'
+config.Data.outputPrimaryDataset = 'ZJToMuMu-H2BugFix-PDFVarExt_pilot'
 config.Data.splitting = 'EventBased'
 config.Data.unitsPerJob = 10
 NJOBS = 10
