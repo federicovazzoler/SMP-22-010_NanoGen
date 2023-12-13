900a8bee9467d466cace51813e40683c5866eeb0
Federico Vazzoler <federico.vazzoler@cern.ch>
2023-12-13 11:55:44 +0100

    Cosmetics

--- crab_submit_files/monitor.sh
+++ crab_submit_files/monitor.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 crab status -d crab_projects/crab_ZJToEE-H2BugFix-PDFVarExt_pilot
-crab status -d crab_projects/crab_ZJToMuMu-H2BugFix-PDFVarExt_pilot
+echo ""
+
+crab status -d crab_projects/crab_ZJToMuMU-H2BugFix-PDFVarExt_pilot
+echo ""
