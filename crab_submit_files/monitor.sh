5af41348ca663d8a9e503e749314c5dee572a2f4
Federico Vazzoler <federico.vazzoler@cern.ch>
2023-12-19 12:17:57 +0100

    Final production scripts

--- crab_submit_files/monitor.sh
+++ crab_submit_files/monitor.sh
@@ -5,22 +5,37 @@ WRKFLD="/nfs/dust/cms/user/vazzolef/NanoGEN/CMSSW_10_6_32_patch1/src/Configurati
 source ${WRKFLD}/setup_naf.sh
 cmsenv
 
-crab status -d ${WRKFLD}/crab_submit_files/crab_projects/crab_ZJToEE-H2BugFix-PDFVarExt_pilot
+clear
+
+#crab status -d ${WRKFLD}/crab_submit_files/crab_projects/crab_ZJToEE-H2BugFix-PDFVarExt_pilot
+#echo ""
+#
+#crab status -d ${WRKFLD}/crab_submit_files/crab_projects/crab_ZJToMuMU-H2BugFix-PDFVarExt_pilot
+#echo ""
+#
+#crab status -d ${WRKFLD}/crab_submit_files/crab_projects/crab_ZJToEE-H2BugFix-PDFVarExt_1000ev_pilot
+#echo ""
+#
+#crab status -d ${WRKFLD}/crab_submit_files/crab_projects/crab_ZJToEE-H2BugFix-PDFVarExt_5000ev_pilot
+#echo ""
+#
+#crab status -d ${WRKFLD}/crab_submit_files/crab_projects/crab_ZJToMuMu-H2BugFix-PDFVarExt_1000ev_pilot
+#echo ""
+#
+#crab status -d ${WRKFLD}/crab_submit_files/crab_projects/crab_ZJToMuMu-H2BugFix-PDFVarExt_5000ev_pilot
+#echo ""
+
+crab status -d ${WRKFLD}/crab_submit_files/crab_projects/crab_ZJToEE-H2BugFix-PDFVarExt
 echo ""
 
-crab status -d ${WRKFLD}/crab_submit_files/crab_projects/crab_ZJToMuMU-H2BugFix-PDFVarExt_pilot
+crab status -d ${WRKFLD}/crab_submit_files/crab_projects/crab_ZJToMuMu-H2BugFix-PDFVarExt
 echo ""
 
-crab status -d ${WRKFLD}/crab_submit_files/crab_projects/crab_ZJToEE-H2BugFix-PDFVarExt_1000ev_pilot
+
+crab status -d ${WRKFLD}/crab_submit_files/crab_projects/crab_ZJToEE-H2BugFix-PDFVarExt_ext1
 echo ""
 
-crab status -d ${WRKFLD}/crab_submit_files/crab_projects/crab_ZJToEE-H2BugFix-PDFVarExt_5000ev_pilot
-echo ""
-
-crab status -d ${WRKFLD}/crab_submit_files/crab_projects/crab_ZJToMuMu-H2BugFix-PDFVarExt_1000ev_pilot
-echo ""
-
-crab status -d ${WRKFLD}/crab_submit_files/crab_projects/crab_ZJToMuMu-H2BugFix-PDFVarExt_5000ev_pilot
+crab status -d ${WRKFLD}/crab_submit_files/crab_projects/crab_ZJToMuMu-H2BugFix-PDFVarExt_ext1
 echo ""
 
 exit 0
--- crab_submit_files/submit.sh
+++ crab_submit_files/submit.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-crab submit crab_submit_Zj_slc7_amd64_gcc10_CMSSW_12_3_1_ZJToEE-suggested-nnpdf31-ncalls-doublefsr-q139-finerGrid_v3-powheg-MiNNLO31-svn3900-ew-rwl6-j200-st2fix-ana-hoppetweights-ymax20-pdf3-PDFVarExt_1000ev_pilot.py
-crab submit crab_submit_Zj_slc7_amd64_gcc10_CMSSW_12_3_1_ZJToEE-suggested-nnpdf31-ncalls-doublefsr-q139-finerGrid_v3-powheg-MiNNLO31-svn3900-ew-rwl6-j200-st2fix-ana-hoppetweights-ymax20-pdf3-PDFVarExt_5000ev_pilot.py
+## Pilot
+#crab submit crab_submit_Zj_slc7_amd64_gcc10_CMSSW_12_3_1_ZJToEE-suggested-nnpdf31-ncalls-doublefsr-q139-finerGrid_v3-powheg-MiNNLO31-svn3900-ew-rwl6-j200-st2fix-ana-hoppetweights-ymax20-pdf3-PDFVarExt_1000ev_pilot.py
+#crab submit crab_submit_Zj_slc7_amd64_gcc10_CMSSW_12_3_1_ZJToEE-suggested-nnpdf31-ncalls-doublefsr-q139-finerGrid_v3-powheg-MiNNLO31-svn3900-ew-rwl6-j200-st2fix-ana-hoppetweights-ymax20-pdf3-PDFVarExt_5000ev_pilot.py
+#
+#crab submit crab_submit_Zj_slc7_amd64_gcc10_CMSSW_12_3_1_ZJToMuMu-suggested-nnpdf31-ncalls-doublefsr-q139-finerGrid_v3-powheg-MiNNLO31-svn3900-ew-rwl6-j200-st2fix-ana-hoppetweights-ymax20-pdf3-PDFVarExt_1000ev_pilot.py
+#crab submit crab_submit_Zj_slc7_amd64_gcc10_CMSSW_12_3_1_ZJToMuMu-suggested-nnpdf31-ncalls-doublefsr-q139-finerGrid_v3-powheg-MiNNLO31-svn3900-ew-rwl6-j200-st2fix-ana-hoppetweights-ymax20-pdf3-PDFVarExt_5000ev_pilot.py
 
-crab submit crab_submit_Zj_slc7_amd64_gcc10_CMSSW_12_3_1_ZJToMuMu-suggested-nnpdf31-ncalls-doublefsr-q139-finerGrid_v3-powheg-MiNNLO31-svn3900-ew-rwl6-j200-st2fix-ana-hoppetweights-ymax20-pdf3-PDFVarExt_1000ev_pilot.py
-crab submit crab_submit_Zj_slc7_amd64_gcc10_CMSSW_12_3_1_ZJToMuMu-suggested-nnpdf31-ncalls-doublefsr-q139-finerGrid_v3-powheg-MiNNLO31-svn3900-ew-rwl6-j200-st2fix-ana-hoppetweights-ymax20-pdf3-PDFVarExt_5000ev_pilot.py
+#######
+
+## First batch 10000 jobs --> 10M ev
+#crab submit crab_submit_Zj_slc7_amd64_gcc10_CMSSW_12_3_1_ZJToEE-suggested-nnpdf31-ncalls-doublefsr-q139-finerGrid_v3-powheg-MiNNLO31-svn3900-ew-rwl6-j200-st2fix-ana-hoppetweights-ymax20-pdf3-PDFVarExt.py
+#crab submit crab_submit_Zj_slc7_amd64_gcc10_CMSSW_12_3_1_ZJToMuMu-suggested-nnpdf31-ncalls-doublefsr-q139-finerGrid_v3-powheg-MiNNLO31-svn3900-ew-rwl6-j200-st2fix-ana-hoppetweights-ymax20-pdf3-PDFVarExt.py
+
+## resubmit first batch and submit new one
+#crab resubmit crab_projects/crab_ZJToEE-H2BugFix-PDFVarExt
+#crab resubmit crab_projects/crab_ZJToMuMu-H2BugFix-PDFVarExt
+
+## Second batch 10000 jobs --> 20M ev
+#crab submit crab_submit_Zj_slc7_amd64_gcc10_CMSSW_12_3_1_ZJToEE-suggested-nnpdf31-ncalls-doublefsr-q139-finerGrid_v3-powheg-MiNNLO31-svn3900-ew-rwl6-j200-st2fix-ana-hoppetweights-ymax20-pdf3-PDFVarExt.py
+#crab submit crab_submit_Zj_slc7_amd64_gcc10_CMSSW_12_3_1_ZJToMuMu-suggested-nnpdf31-ncalls-doublefsr-q139-finerGrid_v3-powheg-MiNNLO31-svn3900-ew-rwl6-j200-st2fix-ana-hoppetweights-ymax20-pdf3-PDFVarExt.py
+
+## resubmit second batch
+crab resubmit crab_projects/crab_ZJToEE-H2BugFix-PDFVarExt_ext1
+crab resubmit crab_projects/crab_ZJToMuMu-H2BugFix-PDFVarExt_ext1
